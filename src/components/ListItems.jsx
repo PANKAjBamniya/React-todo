2bff238070657c7504433fea99b8042ad099ca30
PANKAjBamniya <bamniya853@gmail.com>
2024-11-05 12:53:40 +0530

    delete-funtion

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 
-const ListItems = ({todos,deleteTodo}) => {
+const ListItems = ({todos}) => {
     
   return (
     <>
        <ul className='p-8 w-[90%] bg-gray-400 shadow-xl dark:bg-zinc-800 rounded flex gap-2 flex-col'>
-          {todos.map((todo,index) => (
+          {todos.map((todo) => (
             <li 
             className='w-full flex items-center justify-between bg-white py-2 rounded px-3'>
-              {todo}
+              {todo.text}
               <div className='flex gap-2'>
               <button
               className='bg-yellow-600 text-white rounded py-1 px-3'>Edit</button>
               <button
-              onClick={ () => {
-                deleteTodo(index)
-              }}
               className='bg-red-500 text-white rounded py-1 px-3'>Delete</button>
               </div>
             </li>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,36 +3,22 @@ import ListItems from './ListItems';
 
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]);
-  const [newtask, setNewTask] = useState("");
-
-  const addTodo = (e) => {
-    e.preventDefault(); 
-    if (newtask) {
-      setTodos([...todos, newtask]);
-      setNewTask("");
-    }
-  };
-
-  const deleteTodo = (index) => {
-    const updatedTodos = todos.filter((_,i) => i !== index); 
-    setTodos(updatedTodos);
-  };
-
-  const handleEvent = (e) => {
-    setNewTask(e.target.value);
-  };
+  const [todos,setTodos] = useState([
+    {id: 1,text: "Wake-UP",},
+    {id: 2,text: "Coding",},
+    {id: 3,text: "Eat",},
+    {id: 4,text: "Sleep",},
+    {id: 5,text: "Repeat",},
+  ]);
 
   return (
     <div className='w-full h-[90vh] flex gap-2 flex-col items-center pt-2 dark:bg-gray-700'>
-      <form className='p-6 w-[90%] bg-gray-400 dark:bg-zinc-800 rounded flex gap-2 flex-col' onSubmit={addTodo}>
+      <form className='p-6 w-[90%] bg-gray-400 dark:bg-zinc-800 rounded flex gap-2 flex-col'>
         <span className='flex gap-2 flex-col'>
           <input
             type="text"
             placeholder='Enter your Task'
             className='py-2 rounded px-5 w-[100%]'
-            value={newtask}
-            onChange={handleEvent}
           />
           <button
             type="submit"
@@ -42,7 +28,7 @@ const Todo = () => {
           </button>
         </span>
       </form>
-      <ListItems todos={todos} deleteTodo={deleteTodo} />
+      <ListItems todos={todos} />
     </div>
   );
 };
