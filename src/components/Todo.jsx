a7fd5ad79008f53903d927b9759aea370875b817
PANKAjBamniya <bamniya853@gmail.com>
2024-11-05 13:48:52 +0530

    adit-function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,13 +3,19 @@ import ListItems from './ListItems';
 
 
 const Todo = () => {
-  const [todos,setTodos] = useState([
-    {id: 1,text: "Wake-UP",},
-    {id: 2,text: "Coding",},
-    {id: 3,text: "Eat",},
-    {id: 4,text: "Sleep",},
-    {id: 5,text: "Repeat",},
-  ]);
+  const [todos,setTodos] = useState([ ]);
+
+  const [newtask, setNewtask] = useState("")
+
+  const handleSubmit = ((e) => {
+    e.preventDefault()
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text: newtask
+    }
+    setTodos([...todos, newTodo])
+    setNewtask("")
+  })
 
   const handleDeleteButton = ((id) => {
     setTodos(todos.filter((Todo) => Todo.id !== id))
@@ -17,12 +23,19 @@ const Todo = () => {
 
   return (
     <div className='w-full h-[90vh] flex gap-2 flex-col items-center pt-2 dark:bg-gray-700'>
-      <form className='p-6 w-[90%] bg-gray-400 dark:bg-zinc-800 rounded flex gap-2 flex-col'>
+      <form
+      onSubmit={handleSubmit}
+      className='p-6 w-[90%] bg-gray-400 dark:bg-zinc-800 rounded flex gap-2 flex-col'>
         <span className='flex gap-2 flex-col'>
           <input
             type="text"
             placeholder='Enter your Task'
             className='py-2 rounded px-5 w-[100%]'
+            onChange={(e) => {
+              setNewtask(e.target.value)
+            }}
+            value={newtask}
+            required
           />
           <button
             type="submit"
