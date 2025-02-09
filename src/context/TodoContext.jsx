bdd8bd3d5db64fd5725822c9a82813c5ed7368ee
PaNkaj <147901370+PANKAjBamniya@users.noreply.github.com>
2025-02-09 15:09:42 +0530

    todos

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -1,22 +1,23 @@
 import { createContext, useReducer } from "react";
 import TodoReducer from "./TodoReducer";
 
-const TodoContext = createContext()
+const TodoContext = createContext();
 
-export const TodoProvider = ({children}) => {
+export const TodoProvider = ({ children }) => {
+  const initialState = {
+    todos: [],
+    edit: { todos: {}, isEdit: false },
+  };
 
-    const initialState = {
-        todos: [{id: 1, text: 'Buy milk', description: "description"}],
-        edit: {todos : {} ,isEdit: false}
-    }
+  const [state, dispatch] = useReducer(TodoReducer, initialState);
 
-    const [state , dispatch] = useReducer(TodoReducer , initialState)
-
-    return(
-        <TodoContext.Provider value={{ todos : state.todos ,edit : state.edit, dispatch}}>
-            {children}
-        </TodoContext.Provider>
-    );
+  return (
+    <TodoContext.Provider
+      value={{ todos: state.todos, edit: state.edit, dispatch }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
 };
 
 export default TodoContext;
